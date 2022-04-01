66b698346350e61cc66b3a96ea46a3c1114428d9
sujii2002 <88040463+sujii2002@users.noreply.github.com>
2022-04-01 12:54:54 +0530

    added header and css

---
+++ header.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+        <div class="header_main">
+		<div class="navBar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php#aboutus">About Us</a></li>
+                <li><a href="index.php#contactus">Contact Us</a></li>
+				<li><a href="staff_login.php">Staff Login</a></li>
+			
+			</ul>
+		</div>
+	 <a href="index.php"><div class="logo-name">
+			<div class="logo">
+             <img class="logo_img" src="img/chase.jpg">
+			</div>
+			
+			<div class="name">
+				<h5> Golden-Ager Bank</h5></a><br>
+				
+			</div>
+        
+           
+			
+	</div>
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <html>
-<title>Online Banking System</title>
+<title>Golden Agerh</title>
 <head>
+   <style>
+       .news_events p
+       {
+           font-weight:bold;
+       }
+       
+
+   </style>
    <link rel="stylesheet" type="text/css" href="css/index.css">
    <link rel="shortcut icon" href="img/chase.jpg">
     </head>
@@ -28,9 +36,10 @@
 <div class="news_events">
     <h4>Tips | Updates | Events</h4><br>
         <ul>
-            <p>First, open an account. Then apply for a debit card to get further details.
- </p><br>
-<p>And finally, proceed for Internet Banking Registration to create your internet banking account.
+            <p>If you are new to the bank go for open account. If you are an existing user, go for fund transfer or apply debit card.
+            <br><br>
+            if you have any queries regarding usage of website contact our customer care.
+ 
 
 </p>
             <br>
@@ -43,7 +52,7 @@
         <ul>
             <a href="customer_reg_form.php"><li>Open Account</li></a>
             <a href="debit_card_form.php"><li>Apply Debit Card</li></a><br>
-            <a href="#" id="ebanking" ><li><div class="ebanking">Internet Banking
+            <a href="#" id="ebanking" ><li><div class="ebanking">Edit Account
                 <div class = "ebanking_options">
                 <ul>
                     <a href="customer_login.php"><li>Login </li></a>
@@ -58,15 +67,15 @@
 </div>
 
         <div id="aboutus" class="about"><span>About Us</span><br><br>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <p style="font-weight:bolder;font-size:17px;color: #2c71b0;"> Welcome to our bank - the bank that implements a modern way of banking and will adapt itself to the rapidly changing scenario in the banking industry.It is most useful for the elderly people who struggle working with the website.This
+            Website provides necessary walk throughs for easy access.
 </p></div>
     
     <div class="disclaimer">
-    <span>Disclaimer !!</spasn><br><br>
-        <p>Our bank does not ask for the details of your account/PIN/password. Therefore any one pretending to be asking you for information from the bank/technical team may be fraudulent entities, so please beware.</p>
-        <p>You should know how to operate net transactions and if you are not familiar you may refrain from doing so. You may seek bank's guidance in this regard. Bank is not responsible for online transactions going wrong.</p>
-        <p>We shall also not be responsible for wrong transactions and wanton disclosure of details by you. Viewing option and transaction option on the net are different. You may exercise your option diligently.</p>
-    </div>
+    <span>Disclaimer !!</span><br><br>
+        <p style="font-size:17px;font-weight:bold;">Our bank does not ask for the details of your account/PIN/password.<br>
+        Therefore any one pretending to be asking you for information from the bank/technical team may be fraudulent entities, so please beware.</p>
+        
 
 
     </div>
